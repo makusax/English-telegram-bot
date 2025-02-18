e3c21721d05915dc38412be9cb1d21ae56475724
makusax <4ndrey2016@gmail.com>
2025-02-18 18:44:21 +0300

    Change something with code menu

--- Telegram-bot.py
+++ Telegram-bot.py
@@ -1,27 +1,41 @@
 import telebot
 from telebot import types
-from secrets import secrets
-from compliment import compliments
 import random
-token = ("8195726179:AAHP1t-YFSkYjJgiZX_y6L8FkV4Np3nke_M")
-bot = telebot.TeleBot(token)
+from compliment import compliments
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из .env файла
+load_dotenv()
+
+# Получаем токен из переменной окружения
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+# Проверка токена
+if not TOKEN:
+    raise ValueError("Токен не найден. Проверьте .env файл.")
+
+# Инициализация бота
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     start_button = types.KeyboardButton("🚀Старт")
     action_button = types.KeyboardButton("Комплимент⛰️")
     markup.add(start_button, action_button)
-    bot.send_message(message.chat.id, "Привет✌️ , {0.first_name} \nВоспользуйся кнопками!".format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, f"Привет✌️, {message.from_user.first_name} \nВоспользуйся кнопками!", reply_markup=markup)
 
-bot.polling(none_stop=True, interval=0)
-
-
-
+# Обработчик текстовых сообщений
 @bot.message_handler(content_types=["text"])
 def buttons(message):
-    if(message.text == "🚀Старт"):
-        bot.send_message(message.chat.id, text="Хорошо , выбери из списка интересующий тебя пункт! \n1.Опредиление уровня \n2.Новые слова \n3.Правила")
-    elif (message.text =="Комплимент⛰️" ):
+    if message.text == "🚀Старт":
+        bot.send_message(message.chat.id, text="Хорошо, выбери из списка интересующий тебя пункт! \n1.Определение уровня💯 \n2.Новые слова📗 \n3.Правила💬")
+    elif message.text == "Комплимент⛰️":
         bot.send_message(message.chat.id, text=f"{random.choice(compliments)}")
     else:
-        bot.send_message(message.chat.id , text = "Я могу отвечать только на нажатие кнопок!")
+        bot.send_message(message.chat.id, text="Я могу отвечать только на нажатие кнопок!")
+
+# Запуск бота
+bot.polling(none_stop=True, interval=0)
